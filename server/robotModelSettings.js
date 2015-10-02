5a77b7d6c348d5d3efee50eb353ac92b70b729a3
unknown <joe@josephbrumaghim.com>
2015-10-02 16:28:05 -0400

    Added start energy Ui

--- server/robotModelSettings.js
+++ server/robotModelSettings.js
@@ -1,12 +1,12 @@
 module.exports = {
   maxRunSpeed : 1,
   maxBoostSpeed : 5,
-  boostingHealthDrain : 1,
-  runningHealthGain : 2,
+  boostingHealthDrain : 2,
+  runningHealthGain : .05,
   runningAcclMultiplier : 0.3,
   boostingAcclMultiplier: 4,
   turnSpeed : .5,
   //these two are multiplied by velocity to determine damage
   playerCollisionDamage: 100, 
-  wallCollisionDamage: 500
+  wallCollisionDamage: 100,
 };
