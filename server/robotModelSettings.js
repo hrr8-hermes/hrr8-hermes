d94ae5276c90e90cd54194cbbb6cb878b12b973e
unknown <joe@josephbrumaghim.com>
2015-10-02 19:57:14 -0400

    Tweak

--- server/robotModelSettings.js
+++ server/robotModelSettings.js
@@ -1,13 +1,12 @@
 module.exports = {
   maxRunSpeed : 2,
   maxBoostSpeed : 5,
-
   boostingHealthDrain : .5,
   runningHealthGain : .25,
   runningAcclMultiplier : 0.4,
   boostingAcclMultiplier: 1.4,
   turnSpeed : 0.3,
   //these two are multiplied by velocity to determine damage
-  playerCollisionDamage: 100,
+  playerCollisionDamage: 100, 
   wallCollisionDamage: 100
 };
