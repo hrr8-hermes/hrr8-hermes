717d647204d9fc84d07887e2889fd01d6f3595ec
leothorp <leo.thorp1@gmail.com>
2015-10-05 15:48:21 -0500

    stylesheet

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//app.use(express.static(__dirname + '/../public'));
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../public'));
+//app.use(express.static(__dirname + '/../client'));
 //app.use(express.static(__dirname + '/../client/testClient'));
 
 app.use('/Assets',express.static(__dirname + '/../assets'));
 app.use('/js/external',express.static(__dirname + '/../external'));
 app.use('/sounds',express.static(__dirname + '/../sounds'));
-
+app.use('/styles',express.static(__dirname + '/../styles'));
 module.exports = app;
