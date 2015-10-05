289fa440359e9b761af6ca049dd81acf17975436
jyoko <jyoko@users.noreply.github.com>
2015-10-05 14:08:33 -0400

    Hack to keep step from looping

--- client/js/robotModel.js
+++ client/js/robotModel.js
@@ -69,7 +69,7 @@ Robot.prototype.startRunning = function(){
   sounds.step.loop = true;
   sounds.step.setVolume(.7);
   if (this.id===socket.id) sounds.step.play();
-  sounds.step.stop(4);
+  sounds.step.stop(2);
   scene.beginAnimation(this.skeleton,25,48,true,1.0); 
   this.isRunning = true; 
 };
